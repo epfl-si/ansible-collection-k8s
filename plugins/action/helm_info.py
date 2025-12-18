eb540d3cdf51bd0097c2ee18c3a5086991eff073
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-12-18 20:59:18 +0100

    [fix] `module_utils` directory placement
    As per https://docs.ansible.com/projects/ansible/latest/dev_guide/developing_module_utilities.html

--- plugins/action/helm_info.py
+++ plugins/action/helm_info.py
@@ -1,4 +1,4 @@
-from ansible_collections.epfl_si.k8s.module_utils.k8s_action_base import K8sActionBase
+from ansible_collections.epfl_si.k8s.plugins.module_utils.k8s_action_base import K8sActionBase
 
 class ActionModule(K8sActionBase):
     WRAPPED_MODULE_NAME = "kubernetes.core.helm_info"
