4259455ac68e6cb2ae0603900553838b2b0d9249
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2026-08-18 18:20:06 +0200

    [feature] Support `epfl_si.actions` API (alternatively to raw Ansible)
    *Now* I remember why we had `expand_vars_fn`: `gitsible`'s private
    `gitlab_rails` action wanted to pass in an implementation based on
    `epfl_si.actions`...
    
    - Let it do so by supporting a `jinja` constructor parameter to `AnsibleVars`
    - Code to `epfl_si.action`'s newer API (≥ 2.5.0) only
    - Raise a `RuntimeError` if a `jinja` constructor argument is passed
      while an older `epfl_si.action` is in play

--- plugins/module_utils/ansible_internals.py
+++ plugins/module_utils/ansible_internals.py
@@ -12,7 +12,7 @@ AnsiballZ.
 
 class AnsibleVars:
     """A bunch of Ansible variables, with optional support for template expansion."""
-    def __init__ (self, vars={}, templar=None):
+    def __init__ (self, vars={}, templar=None, jinja=None):
         """Class constructor.
 
         @param vars     The vars dict from the current task or module.
@@ -20,16 +20,30 @@ class AnsibleVars:
         @param templar  The `ansible.template.Templar` instance that should be
                         used to expand vars. Only required (or meaningful)
                         on the Ansible controller (not in a module).
+
+        @param jinja    An instance of the `AnsibleJinja` class from the
+                        `epfl_si.actions` collection. Requires `epfl_si.actions`
+                        version 2.5.0 or above. Mutually
+                        exclusive with both `vars` and `templar` (as
+                        instances of that class encapsulate both).
         """
 
         self._vars = vars
         self._templar = templar
+        self._jinja = jinja
+        if self._jinja is not None and not hasattr(self._jinja, "resolve_variable"):
+            raise RuntimeError("epfl_si.actions version 2.5.0 or above is required for the `jinja` constructor parameter")
 
     def has (self, key):
-        return key in self._vars
+        if self._jinja is not None:
+            return key in self._jinja.vars
+        else:
+            return key in self._vars
 
     def expand (self, var_name):
-        if self._templar is None:
+        if self._jinja is not None:
+            return self._jinja.resolve_variable(var_name)
+        elif self._templar is None:
             return self._vars[var_name]
         elif hasattr(self._templar, "resolve_variable_expression"):
             return self._templar.resolve_variable_expression(var_name)
